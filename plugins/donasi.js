b6015222f349f907d997f31c4134090c37be5926
Pojan16 <89253879+Pojan16@users.noreply.github.com>
2021-08-24 00:14:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -19,7 +19,7 @@ let donate = `
 Ngetik Doang Donasinya Mana?
 `.trim() // Tambah sendiri kalo mau
 conn.send2Button(m.chat, pp, 'pp.jpg', donate, 'Donasi gak akan buatmu miskin kawan:)', 'IYA INSYAALLAH', '/mention Tapi Yaudalah Gue Biarin', 'NGGAK', '/mention Halah Dajjal')
-
+}
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
