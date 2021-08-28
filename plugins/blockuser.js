764b7aee906cf1067401fbf912146c63f8ed6ce4
Pojan16 <89253879+Pojan16@users.noreply.github.com>
2021-08-29 03:34:24 +0700

    Update blockuser.js

--- plugins/blockuser.js
+++ plugins/blockuser.js
@@ -1,20 +1,17 @@
-let handler = async (m, { conn, args}) => {
+let handler = async (m, { conn, args, text}) => {
     if (args.length > 0) {
         let mention = args[0].replace(/[@.+-]/g, '').replace(' ', '')
         let who = (mention + '@s.whatsapp.net')
-        let block = this.blockUser(from, who)
-        if (block = false) {
-          this.blockUser(from, who, 'add') = true
-          conn.reply(m.chat, 'Berhasil memblokir', block, m)
-        } else if (block = true) {
-          this.blockUser(from, who, 'add') = true
-          conn.reply(m.chat, 'Dia sudah keblokir komandan:/', m)
-        }
-    } else conn.reply(m.chat, 'Mau blokir siapa kamu?', m)
+        let block = conn.blockUser(who)// mama mia lezatos~ 👌
+        if (!text) throw 'Kamu mau blokir siapa?'
+        if (!who) throw 'Masukan nomernya, atau tag orangnya!'
+        conn.blockUser(who, 'add')
+        conn.reply(m.chat, `Berhasil memblokir ${who}`, m)
+    }
 }
 handler.help = ['block @mention']
 handler.tags = ['owner']
 handler.command = /^bloc?k(ir)$/i
 handler.owner = true
-
+// we em: Ozan Gans:v
 module.exports = handler
