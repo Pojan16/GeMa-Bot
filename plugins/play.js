c42137d0ee9df7b1eac310adf928b015d6b77b5c
Pojan16 <89253879+Pojan16@users.noreply.github.com>
2021-09-05 10:22:14 +0700

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -3,18 +3,20 @@ let yts = require('yt-search')
 let fetch = require('node-fetch')
 const { servers, yta, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?\n\n#play untuk musik\n#play2 untuk video'
+  if (!text) throw 'Mau cari apa? gunakan\n\n#play dan masukkan text yang mau dicari'
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
+  if (!vid) throw 'Permintaan Tersebut Tidak ditemukan!'
   let isVideo = /2$/.test(command)
   let yt = false
+  let yt2 = false
   let usedServer = servers[0]
   for (let i in servers) {
     let server = servers[i]
     try {
-      yt = await (isVideo ? ytv : yta)(vid.url, server)
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
       usedServer = server
       break
     } catch (e) {
@@ -22,26 +24,14 @@ let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
     }
   }
   if (yt === false) throw 'Semua server tidak bisa :/'
+  if (yt2 === false) throw 'Semua server tidak bisa :/'
   let { dl_link, thumb, title, filesize, filesizeF } = yt
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, `
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
 *Title:* ${title}
-*File size:* ${filesizeF}
+*File Size Audio:* ${filesizeF}
+*File Size Video:* ${yt2.filesizeF}
 *Server y2mate:* ${usedServer}
-*Source:* ${vid.url}
-`.trim(), m)
-let _thumb = {}
-try { if (isVideo) _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-catch (e) { }
-if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*Source:* ${vid.url}
-*Server y2mate:* ${usedServer}
-`.trim(), m, false,  {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+`.trim(), 'Youtube Downloader From GeMa-Bot', 'Audio', `/ytmp3 ${vid.url}`, 'Video', `/ytmp4 ${vid.url}`)
 }
 handler.help = ['play [musik]','play2 [video]']
 handler.tags = ['download']
